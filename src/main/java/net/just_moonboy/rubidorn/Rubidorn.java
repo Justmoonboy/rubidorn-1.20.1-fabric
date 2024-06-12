0996d1a8ffb3b142d4155c7ab1647c7d01729458
Justmoonboy <130238647+Justmoonboy@users.noreply.github.com>
2024-06-12 14:18:02 -0500

    Initial commit

---
+++ src/main/java/net/just_moonboy/rubidorn/Rubidorn.java
@@ -0,0 +1,17 @@
+package net.just_moonboy.rubidorn;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Rubidorn implements ModInitializer {
+	public  static final String MOD_ID = "rubidorn";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/just_moonboy/rubidorn/RubidornClient.java
@@ -0,0 +1,10 @@
+package net.just_moonboy.rubidorn;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubidornClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
